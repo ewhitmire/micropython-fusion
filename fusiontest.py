9c6b0f563dba263f994a9d3d6a32ad8765f24c30
lki1354 <lki1354@students.fhv.at>
2016-10-28 12:59:42 +0200

    change use of pyb to machine * because I want to use the module on the wipy2.0 board * todo check if in new wipy2.0 version ticks_us are implemented

--- fusiontest.py
+++ fusiontest.py
@@ -1,12 +1,17 @@
 # Simple test program for sensor fusion on Pyboard
-# Author Peter Hinch
+# Author: Lukas K.
+# coauthor: Peter Hinch
 # V0.7 25th June 2015 Adapted for new MPU9x50 interface
 
-import pyb
-from mpu9150 import MPU9150
+from machine import I2C, Pin
+import time
+from mpu9250 import MPU9250
 from fusion import Fusion
 
-imu = MPU9150('X')
+#sw_pin = Pin(Pin.exp_board.G17, mode=Pin.IN, pull=Pin.PULL_UP)
+
+i2c = I2C(0, I2C.MASTER, baudrate=400000)
+imu = MPU9250(i2c)
 
 fuse = Fusion()
 
@@ -14,22 +19,41 @@ fuse = Fusion()
 Calibrate = True
 Timing = False
 
-def getmag():                               # Return (x, y, z) tuple (blocking read)
+time_calibration = time.time() + 20
+
+def getmag():
+    """
+    Return (x, y, z) tuple (blocking read)
+    """
     return imu.mag.xyz
+
+
+def switch():
+    if time.time() > time_calibration:
+        return True
+    else:
+        return False
+    # todo can't use the button on the expander board because this is used for the I2C interface
+    #pressed = Pin.exp_board.G17.value()
+    #if pressed:
+    #    pressed = False
+    #else:
+    #    pressed = True
+    #return pressed
+
 
 if Calibrate:
     print("Calibrating. Press switch when done.")
-    sw = pyb.Switch()
-    fuse.calibrate(getmag, sw, lambda : pyb.delay(100))
+    fuse.calibrate(getmag, switch, lambda: time.sleep_ms(100))
     print(fuse.magbias)
 
 if Timing:
-    mag = imu.mag.xyz # Don't include blocking read in time
-    accel = imu.accel.xyz # or i2c
+    mag = imu.mag.xyz  # Don't include blocking read in time
+    accel = imu.accel.xyz  # or i2c
     gyro = imu.gyro.xyz
-    start = pyb.micros()
+    start = time.ticks_ms()
     fuse.update(accel, gyro, mag) # 1.65mS on Pyboard
-    t = pyb.elapsed_micros(start)
+    t = time.ticks_ms() - start
     print("Update time (uS):", t)
 
 count = 0
@@ -37,5 +61,5 @@ while True:
     fuse.update(imu.accel.xyz, imu.gyro.xyz, imu.mag.xyz) # Note blocking mag read
     if count % 50 == 0:
         print("Heading, Pitch, Roll: {:7.3f} {:7.3f} {:7.3f}".format(fuse.heading, fuse.pitch, fuse.roll))
-    pyb.delay(20)
+    time.sleep_ms(20)
     count += 1
