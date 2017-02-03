185d63e2f6913f84938d95dbc8d82cd687014c13
Eric Whitmire <emwhit@cs.washington.edu>
2017-02-02 16:00:45 -0800

    added gyro drift compensation

--- fusion.py
+++ fusion.py
@@ -8,6 +8,9 @@
 
 import time
 from math import sqrt, atan2, asin, degrees, radians
+
+import numpy as np
+
 """
 Supports 6 and 9 degrees of freedom sensors. Tested with InvenSense MPU-9150 9DOF sensor.
 Source https://github.com/xioTechnologies/Open-Source-AHRS-With-x-IMU.git
@@ -32,23 +35,35 @@ class Fusion(object):
     """
     declination = 0                         # Optional offset for true north. A +ve value adds to heading
     def __init__(self):
+        self.magmin = [0, 0, 0]
+        self.magmax = [0, 0, 0]
         self.magbias = (0, 0, 0)            # local magnetic bias factors: set from calibration
         self.start_time = None              # Time between updates
         self.q = [1.0, 0.0, 0.0, 0.0]       # vector to hold quaternion
         GyroMeasError = radians(40)         # Original code indicates this leads to a 2 sec response time
         self.beta = sqrt(3.0 / 4.0) * GyroMeasError  # compute beta (see README)
+        GyroDriftError = radians(5)         # Original code indicates this leads to a 2 sec response time
+        self.zeta = sqrt(3.0 / 4.0) * GyroDriftError  # compute zeta (see README)
+        self.w_bx_ = 0
+        self.w_by_ = 0
+        self.w_bz_ = 0
+
+
+
 
     def calibrate(self, getxyz, stopfunc, waitfunc = None):
-        magmax = list(getxyz())             # Initialise max and min lists with current values
-        magmin = magmax[:]
+        self.magmax = list(getxyz())             # Initialise max and min lists with current values
+        self.magmin = self.magmax[:]
         while not stopfunc():
             if waitfunc is not None:
                 waitfunc()
             magxyz = tuple(getxyz())
             for x in range(3):
-                magmax[x] = max(magmax[x], magxyz[x])
-                magmin[x] = min(magmin[x], magxyz[x])
-        self.magbias = tuple(map(lambda a, b: (a +b)/2, magmin, magmax))
+                self.magmax[x] = max(self.magmax[x], magxyz[x])
+                self.magmin[x] = min(self.magmin[x], magxyz[x])
+
+        self.magbias = tuple(map(lambda a, b: (a + b)/2, self.magmin, self.magmax))
+
 
     @property
     def heading(self):
@@ -64,11 +79,11 @@ class Fusion(object):
         return degrees(atan2(2.0 * (self.q[0] * self.q[1] + self.q[2] * self.q[3]),
             self.q[0] * self.q[0] - self.q[1] * self.q[1] - self.q[2] * self.q[2] + self.q[3] * self.q[3]))
 
-    def update_nomag(self, accel, gyro):    # 3-tuples (x, y, z) for accel, gyro
+    def update_nomag(self, timestamp, accel, gyro):    # 3-tuples (x, y, z) for accel, gyro
         ax, ay, az = accel                  # Units G (but later normalised)
         gx, gy, gz = (radians(x) for x in gyro) # Units deg/s
         if self.start_time is None:
-            self.start_time = time.ticks_ms()  # First run
+            self.start_time = timestamp  # First run
         q1, q2, q3, q4 = (self.q[x] for x in range(4))   # short name local variable for readability
         # Auxiliary variables to avoid repeated arithmetic
         _2q1 = 2 * q1
@@ -112,8 +127,8 @@ class Fusion(object):
         qDot4 = 0.5 * (q1 * gz + q2 * gy - q3 * gx) - self.beta * s4
 
         # Integrate to yield quaternion
-        deltat = (time.ticks_ms() - self.start_time) / 1000     # calculate the elapsed time
-        self.start_time = time.ticks_ms()
+        deltat = (timestamp - self.start_time) / 1000  # calculate the elapsed time
+        self.start_time = timestamp
         q1 += qDot1 * deltat
         q2 += qDot2 * deltat
         q3 += qDot3 * deltat
@@ -121,10 +136,16 @@ class Fusion(object):
         norm = 1 / sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4)    # normalise quaternion
         self.q = q1 * norm, q2 * norm, q3 * norm, q4 * norm
 
-    def update(self, timestamp, accel, gyro, mag):     # 3-tuples (x, y, z) for accel, gyro and mag data
+    def update_old(self, timestamp, accel, gyro, mag):     # 3-tuples (x, y, z) for accel, gyro and mag data
+
+        for i, m in enumerate(mag):
+            self.magmax[i] = max(self.magmax[i], m)
+            self.magmin[i] = min(self.magmin[i], m)
+        self.magbias = tuple(map(lambda a, b: (a + b)/2, self.magmin, self.magmax))
         mx, my, mz = (mag[x] - self.magbias[x] for x in range(3)) # Units irrelevant (normalised)
         ax, ay, az = accel                  # Units irrelevant (normalised)
         gx, gy, gz = (radians(x) for x in gyro)  # Units deg/s
+        # gz = -gz
         if self.start_time is None:
             self.start_time = timestamp  # First run
         q1, q2, q3, q4 = (self.q[x] for x in range(4))   # short name local variable for readability
@@ -215,3 +236,125 @@ class Fusion(object):
         q4 += qDot4 * deltat
         norm = 1 / sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4)    # normalise quaternion
         self.q = q1 * norm, q2 * norm, q3 * norm, q4 * norm
+
+
+    def update(self, timestamp, accel, gyro, mag):
+        for i, m in enumerate(mag):
+            self.magmax[i] = max(self.magmax[i], m)
+            self.magmin[i] = min(self.magmin[i], m)
+
+        self.magbias = tuple(map(lambda a, b: (a + b) / 2, self.magmin, self.magmax))
+        mx, my, mz = (mag[x] - self.magbias[x] for x in range(3))  # Units irrelevant (normalised)
+        ax, ay, az = accel  # Units irrelevant (normalised)
+        gx, gy, gz = (radians(x) for x in gyro)  # Units deg/s
+        q0, q1, q2, q3 = (self.q[x] for x in range(4))   # short name local variable for readability
+        # float recipNorm
+        # float s0, s1, s2, s3
+        # float qDot1, qDot2, qDot3, qDot4
+        # float hx, hy
+        # float _2q0mx, _2q0my, _2q0mz, _2q1mx, _2bx, _2bz, _4bx, _4bz, _2q0, _2q1, _2q2, _2q3, _2q0q2, _2q2q3, q0q0, q0q1, q0q2, q0q3, q1q1, q1q2, q1q3, q2q2, q2q3, q3q3
+
+
+        if self.start_time is None:
+            self.start_time = timestamp  # First run
+
+        deltat = (timestamp - self.start_time) / 1000     # calculate the elapsed time
+        self.start_time = timestamp
+
+        # Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+        if not ((ax == 0.0) and (ay == 0.0) and (az == 0.0)):
+
+            # Normalise accelerometer measurement
+            recipNorm = 1 / sqrt(ax * ax + ay * ay + az * az)
+            ax *= recipNorm
+            ay *= recipNorm
+            az *= recipNorm
+
+            # Normalise magnetometer measurement
+            recipNorm = 1 / sqrt(mx * mx + my * my + mz * mz)
+            mx *= recipNorm
+            my *= recipNorm
+            mz *= recipNorm
+
+            # Auxiliary variables to avoid repeated arithmetic
+            _2q0mx = 2.0 * q0 * mx
+            _2q0my = 2.0 * q0 * my
+            _2q0mz = 2.0 * q0 * mz
+            _2q1mx = 2.0 * q1 * mx
+            _2q0 = 2.0 * q0
+            _2q1 = 2.0 * q1
+            _2q2 = 2.0 * q2
+            _2q3 = 2.0 * q3
+            _2q0q2 = 2.0 * q0 * q2
+            _2q2q3 = 2.0 * q2 * q3
+            q0q0 = q0 * q0
+            q0q1 = q0 * q1
+            q0q2 = q0 * q2
+            q0q3 = q0 * q3
+            q1q1 = q1 * q1
+            q1q2 = q1 * q2
+            q1q3 = q1 * q3
+            q2q2 = q2 * q2
+            q2q3 = q2 * q3
+            q3q3 = q3 * q3
+
+            # Reference direction of Earth's magnetic field
+            hx = mx * q0q0 - _2q0my * q3 + _2q0mz * q2 + mx * q1q1 + _2q1 * my * q2 + _2q1 * mz * q3 - mx * q2q2 - mx * q3q3
+            hy = _2q0mx * q3 + my * q0q0 - _2q0mz * q1 + _2q1mx * q2 - my * q1q1 + my * q2q2 + _2q2 * mz * q3 - my * q3q3
+            _2bx = sqrt(hx * hx + hy * hy)
+            _2bz = -_2q0mx * q2 + _2q0my * q1 + mz * q0q0 + _2q1mx * q3 - mz * q1q1 + _2q2 * my * q3 - mz * q2q2 + mz * q3q3
+            _4bx = 2.0 * _2bx
+            _4bz = 2.0 * _2bz
+
+            # Gradient decent algorithm corrective step
+            s0 = -_2q2 * (2.0 * q1q3 - _2q0q2 - ax) + _2q1 * (2.0 * q0q1 + _2q2q3 - ay) - _2bz * q2 * (_2bx * (0.5 - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (-_2bx * q3 + _2bz * q1) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + _2bx * q2 * (_2bx * (q0q2 + q1q3) + _2bz * (0.5 - q1q1 - q2q2) - mz)
+            s1 = _2q3 * (2.0 * q1q3 - _2q0q2 - ax) + _2q0 * (2.0 * q0q1 + _2q2q3 - ay) - 4.0 * q1 * (1 - 2.0 * q1q1 - 2.0 * q2q2 - az) + _2bz * q3 * (_2bx * (0.5 - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (_2bx * q2 + _2bz * q0) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + (_2bx * q3 - _4bz * q1) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5 - q1q1 - q2q2) - mz)
+            s2 = -_2q0 * (2.0 * q1q3 - _2q0q2 - ax) + _2q3 * (2.0 * q0q1 + _2q2q3 - ay) - 4.0 * q2 * (1 - 2.0 * q1q1 - 2.0 * q2q2 - az) + (-_4bx * q2 - _2bz * q0) * (_2bx * (0.5 - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (_2bx * q1 + _2bz * q3) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + (_2bx * q0 - _4bz * q2) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5 - q1q1 - q2q2) - mz)
+            s3 = _2q1 * (2.0 * q1q3 - _2q0q2 - ax) + _2q2 * (2.0 * q0q1 + _2q2q3 - ay) + (-_4bx * q3 + _2bz * q1) * (_2bx * (0.5 - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (-_2bx * q0 + _2bz * q2) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + _2bx * q1 * (_2bx * (q0q2 + q1q3) + _2bz * (0.5 - q1q1 - q2q2) - mz)
+            recipNorm = 1 / sqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3) # normalise step magnitude
+            s0 *= recipNorm
+            s1 *= recipNorm
+            s2 *= recipNorm
+            s3 *= recipNorm
+
+            _w_err_x = _2q0 * s1 - _2q1 * s0 - _2q2 * s3 + _2q3 * s2
+            _w_err_y = _2q0 * s2 + _2q1 * s3 - _2q2 * s0 - _2q3 * s1
+            _w_err_z = _2q0 * s3 - _2q1 * s2 + _2q2 * s1 - _2q3 * s0
+
+            self.w_bx_ += _w_err_x * deltat * self.zeta
+            self.w_by_ += _w_err_y * deltat * self.zeta
+            self.w_bz_ += _w_err_z * deltat * self.zeta
+
+            gx -= self.w_bx_
+            gy -= self.w_by_
+            gz -= self.w_bz_
+
+
+            # Rate of change of quaternion from gyroscope
+            qDot1 = 0.5 * (-q1 * gx - q2 * gy - q3 * gz)
+            qDot2 = 0.5 * (q0 * gx + q2 * gz - q3 * gy)
+            qDot3 = 0.5 * (q0 * gy - q1 * gz + q3 * gx)
+            qDot4 = 0.5 * (q0 * gz + q1 * gy - q2 * gx)
+
+            # Apply feedback step
+            qDot1 -= self.beta * s0
+            qDot2 -= self.beta * s1
+            qDot3 -= self.beta * s2
+            qDot4 -= self.beta * s3
+
+            # Integrate rate of change of quaternion to yield quaternion
+
+
+            q0 += qDot1 * deltat
+            q1 += qDot2 * deltat
+            q2 += qDot3 * deltat
+            q3 += qDot4 * deltat
+
+            # Normalise quaternion
+            recipNorm = 1 / sqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3)
+            q0 *= recipNorm
+            q1 *= recipNorm
+            q2 *= recipNorm
+            q3 *= recipNorm
+
+            self.q = q0, q1, q2, q3
